7e93f3f346b7c3a6087c6dc838eb2de07157eddd
AleNerima <ale95polioni@gmail.com>
2024-05-22 15:43:26 +0200

    crati componenti

---
+++ settimana 1/giorno 3/EpicodeBlog/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ActivePostsComponent } from './Components/active-posts/active-posts.component';
+import { InactivePostsComponent } from './Components/inactive-posts/inactive-posts.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { PostDetailComponent } from './Components/post-detail/post-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ActivePostsComponent,
+    InactivePostsComponent,
+    NavbarComponent,
+    PostDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
