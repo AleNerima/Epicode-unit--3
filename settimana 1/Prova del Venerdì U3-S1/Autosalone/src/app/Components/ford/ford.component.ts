130fc6622eb4d8d346619add89130f4fb9a976b0
AleNerima <ale95polioni@gmail.com>
2024-05-24 15:12:29 +0200

    correzioni grafiche footer

--- settimana 1/Prova del Venerdì U3-S1/Autosalone/src/app/Components/ford/ford.component.ts
+++ settimana 1/Prova del Venerdì U3-S1/Autosalone/src/app/Components/ford/ford.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { iVeicolo } from '../../Models/veicolo';
+import { DataService } from '../../asset-auto-service.service';
 
 @Component({
   selector: 'app-ford',
   templateUrl: './ford.component.html',
-  styleUrl: './ford.component.scss'
+  styleUrls: ['./ford.component.scss']
 })
-export class FordComponent {
+export class FordComponent implements OnInit {
+  veicoli: iVeicolo[] = [];
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.caricaVeicoli();
+  }
+
+  async caricaVeicoli(): Promise<void> {
+    this.veicoli = await this.dataService.getVeicoliByMarca('Ford');
+  }
 }
