d847262606a5119d9fff5806c20e3e3c93a3b82b
AleNerima <ale95polioni@gmail.com>
2024-05-24 12:28:00 +0200

    attivate le varie route, inizio lavoro su homepage

--- settimana 1/Prova del Venerdì U3-S1/Autosalone/src/app/app-routing.module.ts
+++ settimana 1/Prova del Venerdì U3-S1/Autosalone/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { FiatComponent } from './Components/fiat/fiat.component';
+import { FordComponent } from './Components/ford/ford.component';
+import { AudiComponent } from './Components/audi/audi.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'fiat', component: FiatComponent },
+  { path: 'ford', component: FordComponent },
+  { path: 'audi', component: AudiComponent },
+  { path: '**', redirectTo:''} // Se la pagina non è stata trovata reindirizzo alla homepage.
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- settimana 1/Prova del Venerdì U3-S1/Autosalone/src/app/app.component.ts
+++ settimana 1/Prova del Venerdì U3-S1/Autosalone/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Autosalone';
+export class AppComponent implements OnInit {
+  isHomePage: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Listener per l'evento di navigazione
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Nasconde la home page quando l'utente naviga verso altre pagine
+        this.isHomePage = (event.url === '/');
+      }
+    });
+  }
 }
