973c952159c0e4f00e4abf1837ea3238d6df87a1
AleNerima <ale95polioni@gmail.com>
2024-05-30 17:05:06 +0200

    corretto interceptor

--- settimana 2/giorno 4/photos/src/app/http-error.interceptor.ts
+++ settimana 2/giorno 4/photos/src/app/http-error.interceptor.ts
@@ -1,9 +1,10 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { iPhotos } from './Interfaces/photos';
 
 export class HttpErrorInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(req: HttpRequest<iPhotos>, next: HttpHandler): Observable<HttpEvent<iPhotos>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         console.error('Errore HTTP:', error);
