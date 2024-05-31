e2df7f1e20429bb60e95d83048eb5b61036c407f
AleNerima <ale95polioni@gmail.com>
2024-05-31 14:12:25 +0200

    completati i componenti, inizio prove su cerca

--- settimana 2/Prova venerdì U3S2 Polioni Alessandro/TodoApp/src/app/Components/task-completati/task-completati.component.ts
+++ settimana 2/Prova venerdì U3S2 Polioni Alessandro/TodoApp/src/app/Components/task-completati/task-completati.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { iTodo } from '../../Models/todo-task';
+import { TodoTaskService } from '../../Services/todo-task.service';
 
 @Component({
   selector: 'app-task-completati',
   templateUrl: './task-completati.component.html',
-  styleUrl: './task-completati.component.scss'
+  styleUrls: ['./task-completati.component.scss']
 })
-export class TaskCompletatiComponent {
+export class TaskCompletatiComponent implements OnInit {
+  completedTasks: iTodo[] | undefined;
 
+  constructor(private todoTaskService: TodoTaskService) { }
+
+  ngOnInit(): void {
+    this.getCompletedTasks();
+  }
+
+  getCompletedTasks(): void {
+    this.completedTasks = this.todoTaskService.getTodoTasks().filter(task => task.completed);
+  }
 }
--- settimana 2/Prova venerdì U3S2 Polioni Alessandro/TodoApp/src/app/Models/todo-task.ts
+++ settimana 2/Prova venerdì U3S2 Polioni Alessandro/TodoApp/src/app/Models/todo-task.ts
@@ -1,5 +1,4 @@
 export interface iTodo {
-
   id: number;
   todo: string;
   completed: boolean;
