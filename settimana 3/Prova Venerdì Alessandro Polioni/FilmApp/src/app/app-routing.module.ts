a4dd295f98f15b3b6418687a32f2c0cbb19abd43
AleNerima <ale95polioni@gmail.com>
2024-06-07 19:26:02 +0200

    prova del venerdi correzioni progetto

---
+++ settimana 3/Prova Venerdì Alessandro Polioni/FilmApp/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
+import { GuestGuard } from './auth/guest.guard';
+import { AuthGuard } from './auth/auth.guard';
+
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./Pages/home/home.module').then(m => m.HomeModule) },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate: [GuestGuard],
+    canActivateChild: [GuestGuard],
+   },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./Pages/favorite/favorite.module').then(m => m.FavoriteModule),
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
